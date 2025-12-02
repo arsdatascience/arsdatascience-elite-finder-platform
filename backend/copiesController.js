bf7b6bc2550159b96d22f6e8c9c69421adc2238c
arsdatascience <arsdatascience@gmail.com>
2025-12-02 19:07:28 -0300

    Fix authenticateToken import in copiesController

--- backend/copiesController.js
+++ backend/copiesController.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const db = require('./db');
-const { authenticateToken } = require('./middleware/auth');
+const authenticateToken = require('./middleware/auth');
 
 // Salvar uma nova copy
 router.post('/', authenticateToken, async (req, res) => {
